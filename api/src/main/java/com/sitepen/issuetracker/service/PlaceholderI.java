bdf45b1ea19c3dc69e908ec26954671e77fc0dc6
Brittany Palac <brittany.a.palac@gmail.com>
2026-02-19 16:44:08 -0500

    Removing unused placeholder code

--- api/src/main/java/com/sitepen/issuetracker/service/PlaceholderI.java
+++
@@ -1,9 +0,0 @@
-package com.sitepen.issuetracker.service;
-
-import com.sitepen.issuetracker.dto.PlaceholderDto;
-
-import java.util.List;
-
-public interface PlaceholderI {
-    public PlaceholderDto getByPlaceholderId(Long id);
-}
